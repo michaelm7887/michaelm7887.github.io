674328e23dcaedcce29ecb8411b1159fb0e38fdc
michaelm7887 <146866669+michaelm7887@users.noreply.github.com>
2024-01-31 12:02:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Michael Mera"; // update the part BETWEEN the "quotes"
+let userName = "michaelm7887"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
